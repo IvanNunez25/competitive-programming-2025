36570fc7231a5c6dafa74020cc31b7adfdcce4ed
Ivanovicx Nuñez <ivan25606@gmail.com>
2025-08-05 19:03:25 -0600

    Initial Commit

---
+++ competitive-programming-2025/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  competitive-programming-2025
+//
+//  Created by Ivanovicx Nuñez on 05/08/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
